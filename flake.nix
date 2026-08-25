e027c25818d76a88b0e05cba16868be6a062cb50
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-25 02:20:16 +0200

    build: use canonical GitHub inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     rs-harbor = {
-      url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
+      url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
       inputs.nix-opencode-lsp.url = "git+ssh://git@github.com/caniko/nix-opencode-lsp.git?ref=trunk";
     };
     nixpkgs.follows = "rs-harbor/nixpkgs";
